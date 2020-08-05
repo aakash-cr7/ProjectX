1197a01014ba1fd3b97cb3d946ce52c7bad81925
Aakash Tyagi <aakash.tyg93@gmail.com>
2020-08-05 16:36:21 +0530

    Initialize project

---
+++ src/components/Test.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Test;
+
+import React from 'react';
+
+function Test(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Test;
